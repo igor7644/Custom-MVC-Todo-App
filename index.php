29ce782f12bf62dae25089316166d65026333849
Igor <igor.milosavljevic.1994@gmail.com>
2019-11-14 13:02:49 +0100

    Request class (request url method)

--- index.php
+++ index.php
@@ -2,5 +2,4 @@
 
 require 'core/bootstrap.php';
 
-$url = trim($_SERVER['REQUEST_URI'], '/');
-require Router::load('routes.php')->direct($url);
+require Router::load('routes.php')->direct(Request::url());
