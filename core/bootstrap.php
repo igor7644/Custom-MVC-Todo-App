8fb6c1ae1218c3bd20b51c68e4ebbb5bcc1d0c63
Igor <igor.milosavljevic.1994@gmail.com>
2019-11-15 17:10:24 +0100

    Set up Composer Autoload

--- core/bootstrap.php
+++ core/bootstrap.php
@@ -1,10 +1,4 @@
 <?php
 
 $config = require 'config.php';
-require 'core/Router.php';
-require 'core/Request.php';
-require 'core/database/Connection.php';
-require 'core/database/QueryBuilder.php';
-require 'functions.php';
-
 $pdo = Connection::make($config['database']);
