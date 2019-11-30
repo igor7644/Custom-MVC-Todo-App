54acf5e55b6b5ce7ec320f7060681021405e5af8
Igor <igor.milosavljevic.1994@gmail.com>
2019-11-30 15:14:17 +0100

    Route with and without params private method (refactor Direct method)

--- core/Router.php
+++ core/Router.php
@@ -24,30 +24,10 @@ class Router{
     {
         if(array_key_exists($url, $this->routes[$requestMethod]))
         {
-            $arr = explode('@', $this->routes[$requestMethod][$url]);
-            $controller = $arr[0];
-            $action = $arr[1];
-            return $this->callAction($controller, $action);
-        }
-        else
-        {
-            foreach($this->routes[$requestMethod] as $key => $controllerAction)
-            {
-                $pattern = "@^".preg_replace('/{([a-zA-Z0-9]+)}/', '(?<$1>[a-zA-Z0-9\_\-]+)', $key)."$@";
-                preg_match($pattern, $url, $matches);
-                array_shift($matches);
-                if($matches)
-                {
-                    $params = $matches;
-                    $getAction = explode('@', $controllerAction);
-                    $controller = $getAction[0];
-                    $action = $getAction[1];
-                    return $this->callAction($controller, $action, $params);
-                }
-            }
+            return $this->routeWithoutParams($url, $requestMethod);
         }
 
-        throw new \Exception("No route defined for {$url} URL!");
+        return $this->routeWithParams($url, $requestMethod);
     }
 
     public static function load($file)
@@ -68,6 +48,32 @@ class Router{
         }
 
         throw new Exception("Method {$action} does not exist in {$controller}!");
+    }
+
+    private function routeWithoutParams($url, $requestMethod)
+    {
+        $controllerAction = explode('@', $this->routes[$requestMethod][$url]);
+        $controller = $controllerAction[0];
+        $action = $controllerAction[1];
+        return $this->callAction($controller, $action);
+    }
+
+    private function routeWithParams($url, $requestMethod)
+    {
+        foreach($this->routes[$requestMethod] as $key => $controllerAction)
+        {
+            $pattern = "@^".preg_replace('/{([a-zA-Z0-9]+)}/', '(?<$1>[a-zA-Z0-9\_\-]+)', $key)."$@";
+            preg_match($pattern, $url, $matches);
+            array_shift($matches);
+            if($matches)
+            {
+                $params = $matches;
+                $getAction = explode('@', $controllerAction);
+                $controller = $getAction[0];
+                $action = $getAction[1];
+                return $this->callAction($controller, $action, $params);
+            }
+        }
     }
 
 }
